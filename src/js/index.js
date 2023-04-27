72257a7d0c8d414f018716040a1fca4e21c60b22
saltysaff <d_ash12345@hotmail.co.uk>
2023-04-27 21:56:55 +0100

    add comments to index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,30 @@
 import { deer as deerData } from "./data.js";
 import Deer from "./Deer.js";
 
+// Get new deer from the data
 const getNewDeer = () => deerData.shift();
 let deer = new Deer(getNewDeer());
 
+// DOM elements
 const rejectBtn = document.querySelector(".reject-btn");
 const acceptBtn = document.querySelector(".accept-btn");
 const acceptStamp = document.querySelector(".like-stamp");
 const rejectStamp = document.querySelector(".nope-stamp");
 const avatar = document.getElementById("avatar");
 
+// Show the appropriate stamp based on the choice
 const renderStamp = (choice) => {
   acceptStamp.style.display = choice ? "block" : "none";
   rejectStamp.style.display = choice ? "none" : "block";
 };
 
+// Reset the visibility of stamps
 const resetStamp = () => {
   acceptStamp.style.display = "none";
   rejectStamp.style.display = "none";
 };
 
+// Render the deer profile and reset stamps
 const render = () => {
   resetStamp();
   document.getElementById("bio").innerHTML = deer.getDeerHtml();
@@ -28,26 +33,26 @@ const render = () => {
 
 render();
 
+// Set the choice (like or dislike) for the current deer
 const setChoice = (choice) => {
   deer.isSwiped = true;
-  if (choice) {
-    deer.isLiked = true;
-  } else {
-    deer.isLiked = false;
-  }
+  deer.isLiked = choice;
 };
 
+// Display the "no matches" message
 const displayNoMatchesMessage = () => {
   document.querySelector(".no-matches-msg").style.display = "block";
 };
 
+// Handle the situation when there are no more matches
 const handleNoMatches = () => {
-    displayNoMatchesMessage();
-    avatar.style.backgroundImage = `linear-gradient(0deg, rgba(0, 0, 0, 0.31), rgba(0, 0, 0, 0.31)), url(${deer.avatar})`;
-    rejectBtn.disabled = true;
-    acceptBtn.disabled = true;
-}
+  displayNoMatchesMessage();
+  avatar.style.backgroundImage = `linear-gradient(0deg, rgba(0, 0, 0, 0.31), rgba(0, 0, 0, 0.31)), url(${deer.avatar})`;
+  rejectBtn.disabled = true;
+  acceptBtn.disabled = true;
+};
 
+// Handle the user's choice (like or dislike)
 const handleChoice = (choice) => {
   renderStamp(choice);
   if (deerData.length === 0) {
